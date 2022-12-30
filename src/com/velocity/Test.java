65d27d74087ccec550c9ccec5cf03d587295d797
akshaybaravkar55 <akshaybaravkar55@gmail.com>
2022-12-30 16:49:59 +0530

    arraylist1

---
+++ src/com/velocity/Test.java
@@ -0,0 +1,14 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+	public static void main(String[] args) {
+		List<String> list =new ArrayList<String>();
+		list.add("om");
+		list.add("ram");
+		System.out.println(list);
+	}
+
+}
